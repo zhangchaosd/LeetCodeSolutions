bc87ea14970ffc8f449b00e6efeb19802534570b
zhangchaosd <zhangchaogoo@gmail.com>
2019-05-02 18:00:04 +0800

    add 142

---
+++ LeetCodeSolutions/LeetCodeSolutions/Solves/S142.hpp
@@ -0,0 +1,51 @@
+﻿
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class S142 {
+	void noo()
+	{
+		printf("no cycle\n");
+	}
+public:
+	ListNode* detectCycle(ListNode* head) {
+		if (head == 0) { noo(); return 0; }
+		if (head->next == 0) { noo(); return 0; }
+		ListNode* p1 = head;
+		ListNode* p2 = head;
+		do
+		{
+			p1 = p1->next;
+			if (p1)
+				p1 = p1->next;
+			if (p1 == 0)
+			{
+				noo(); return 0;
+			}
+			p2 = p2->next;
+		} while (p1 != p2);
+		ListNode * ans = head;
+		int op = 0;
+		while (ans != p2)
+		{
+			ans = ans->next;
+			p2 = p2->next;
+			op++;
+		}
+		printf("tail connects to node index %d", op);
+		return ans;
+	}
+};
+
--- LeetCodeSolutions/LeetCodeSolutions/Source.cpp
+++ LeetCodeSolutions/LeetCodeSolutions/Source.cpp
@@ -44,6 +44,7 @@
 #include "Solves\S125.hpp"
 #include "Solves\S138.hpp"
 #include "Solves\S141.hpp"
+#include "Solves\S142.hpp"
 //#include "Solves\S146.hpp"
 //#include "Solves\S151.hpp"
 //#include "Solves\S153.hpp"
